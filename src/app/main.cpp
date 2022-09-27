b58bccb513b7478ab73ded92a4bd9e346a693e18
Kirill Ryabov <sailor.kirill@gmail.com>
2022-09-28 01:24:47 +0700

    Remove get_string_ngrams function

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,18 +5,6 @@
 #include <CLI/Formatter.hpp>
 #include <fmt/core.h>
 
-static std::string
-get_string_ngrams(const std::vector<libfts::ParsedString> &words) {
-    std::string string_ngrams;
-    for (const auto &word : words) {
-        size_t pos = word.text_position_;
-        for (const auto &ngram : word.ngrams_) {
-            string_ngrams += ngram + " " + std::to_string(pos) + " ";
-        }
-    }
-    return string_ngrams;
-}
-
 int main(int argc, char **argv) {
     CLI::App app{"wow, it's working!!"};
     std::string text;
@@ -30,7 +18,13 @@ int main(int argc, char **argv) {
     try {
         libfts::ParserConfiguration config = libfts::load_config();
         std::vector<libfts::ParsedString> result = libfts::parse(text, config);
-        fmt::print("{}\n", get_string_ngrams(result));
+        for (const auto &word : result) {
+            fmt::print("position in text: {}\nngrams: ", word.text_position_);
+            for (const auto &ngram : word.ngrams_) {
+                fmt::print("{} ", ngram);
+            }
+            fmt::print("\n\n");
+        }
     } catch (libfts::ConfigurationException &ex) {
         fmt::print("parse error: {}\n", ex.what());
         return -1;
