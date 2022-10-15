805ebc1b5a7319ad62a797dfddc7a993eb0791c9
Kirill Ryabov <sailor.kirill@gmail.com>
2022-10-15 18:55:16 +0700

    Add a global variable containing the absolute path to the project root

--- tests/libfts/indexer.test.cpp
+++ tests/libfts/indexer.test.cpp
@@ -1,3 +1,4 @@
+#include <libfts/absolutepath.hpp>
 #include <libfts/indexer.hpp>
 #include <libfts/parser.hpp>
 
@@ -5,13 +6,13 @@
 
 TEST(IndexerTest, Indexer) {
     libfts::ParserConfiguration config =
-        libfts::load_config("../../../ParserConfig.json");
+        libfts::load_config(ABSOLUTE_PATH + "/ParserConfig.json");
     libfts::IndexBuilder idx;
     idx.add_document(199903, "The Matrix matrix awwww", config);
     idx.add_document(200305, "The Matrix Reloaded", config);
     idx.add_document(200311, "The Matrix Revolutions", config);
     libfts::TextIndexWriter writer;
-    writer.write("../../../index/", idx.get_index());
+    writer.write(ABSOLUTE_PATH + "/index/", idx.get_index());
     idx.add_document(200458, "The Matrix in the red bottoms MATR", config);
-    writer.write("../../../index/", idx.get_index());
+    writer.write(ABSOLUTE_PATH + "/index/", idx.get_index());
 }
