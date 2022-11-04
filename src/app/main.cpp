7608a5ee13d984a23fb9913c79b6799daef5de82
Kirill Ryabov <sailor.kirill@gmail.com>
2022-11-04 20:36:43 +0700

    Add replxx lib to make interactive mode

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,22 +4,35 @@
 #include <CLI/Config.hpp>
 #include <CLI/Formatter.hpp>
 #include <fmt/core.h>
+#include <replxx.hxx>
 
 int main(int argc, char **argv) {
-    CLI::App app{"wow, it's working!!"};
-    std::string text;
-    app.add_option<std::string>("-t,--text", text, "Parse typed text")
+    CLI::App app{"fts"};
+    auto *indexer_sub = app.add_subcommand("index", "Indexer");
+    auto *searcher_sub = app.add_subcommand("search", "Searcher");
+    std::string path;
+    std::string query;
+    indexer_sub
+        ->add_option<std::string>(
+            "-i,--index", path, "path to folder with indexes")
         ->required();
+    searcher_sub
+        ->add_option<std::string>(
+            "-i,--index", path, "path to folder with indexes")
+        ->required();
+    searcher_sub->add_option<std::string>("-q,--query", query, "search query");
     CLI11_PARSE(app, argc, argv);
     try {
-        libfts::ParserConfiguration config = libfts::load_config();
-        std::vector<libfts::ParsedString> result = libfts::parse(text, config);
-        for (const auto &word : result) {
-            fmt::print("position in text: {}\nngrams: ", word.text_position_);
-            for (const auto &ngram : word.ngrams_) {
-                fmt::print("{} ", ngram);
+        if (indexer_sub->parsed()) {
+            fmt::print("indexer!!!!\n");
+        } else if (searcher_sub->parsed()) {
+            fmt::print("searcher!!!!\n");
+            if (searcher_sub->count("--query") != 1) {
+                replxx::Replxx editor;
+                fmt::print("input query text\n");
+                query = editor.input("> ");
             }
-            fmt::print("\n\n");
+            fmt::print("query: {}\n", query);
         }
     } catch (libfts::ConfigurationException &ex) {
         fmt::print("parse error: {}\n", ex.what());
