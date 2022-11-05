2919881ea2bdf6d83948bb7fe66eece67099e1fd
Kirill Ryabov <sailor.kirill@gmail.com>
2022-11-05 17:43:01 +0700

    Distribute by function the implementation of modes

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -8,6 +8,70 @@
 #include <fmt/core.h>
 #include <replxx.hxx>
 
+static void launch_indexer(
+    const libfts::ParserConfiguration &config,
+    const std::filesystem::path &path) {
+    libfts::IndexBuilder indexer;
+    const size_t column_count = 3;
+    io::CSVReader<column_count> csv(path / "books.csv");
+    csv.read_header(
+        io::ignore_extra_column, "bookID", "title", "language_code");
+    libfts::DocId document_id = 0;
+    libfts::Doc text;
+    std::string language;
+    while (csv.read_row(document_id, text, language)) {
+        if (language == "eng" || language == "en-US") {
+            indexer.add_document(document_id, text, config);
+        }
+    }
+    libfts::TextIndexWriter::write(path / "index", indexer.get_index());
+}
+
+static void launch_searcher(
+    const libfts::ParserConfiguration &config,
+    const std::filesystem::path &path,
+    std::string &query) {
+    try {
+        libfts::IndexAccessor accessor(
+            libfts::TextIndexReader::read(path / "index"));
+        auto result = libfts::search(query, config, accessor);
+        fmt::print("{}", libfts::get_string_search_result(result));
+    } catch (libfts::AccessorException &ex) {
+        fmt::print("error: {}\n", ex.what());
+    }
+}
+
+static void launch_interactive_searcher(
+    const libfts::ParserConfiguration &config,
+    const std::filesystem::path &path,
+    std::string &query) {
+    replxx::Replxx editor;
+    while (true) {
+        char const *cinput{nullptr};
+        do {
+            cinput = editor.input("> ");
+        } while ((cinput == nullptr) && (errno == EAGAIN));
+        if (cinput == nullptr) {
+            break;
+        }
+        query = cinput;
+        if (query.empty()) {
+            /* user hits enter on an empty line */
+            continue;
+        }
+        if (query.compare(0, 5, ".quit") == 0 ||
+            query.compare(0, 5, ".exit") == 0) {
+            break;
+        }
+        if (query.compare(0, 6, ".clear") == 0) {
+            /* clear the screen */
+            editor.clear_screen();
+            continue;
+        }
+        launch_searcher(config, path, query);
+    }
+}
+
 int main(int argc, char **argv) {
     CLI::App app{"fts"};
     auto *indexer_sub = app.add_subcommand("index", "Indexer");
@@ -25,41 +89,19 @@ int main(int argc, char **argv) {
     searcher_sub->add_option<std::string>("-q,--query", query, "search query");
     CLI11_PARSE(app, argc, argv);
     try {
-        const auto config =
-            libfts::load_config(c_absolute_path / "ParserConfig.json");
+        std::filesystem::path path(c_absolute_path);
+        const auto config = libfts::load_config(path / "ParserConfig.json");
         if (indexer_sub->parsed()) {
-            libfts::IndexBuilder indexer;
-            const size_t column_count = 3;
-            io::CSVReader<column_count> csv(c_absolute_path / "books.csv");
-            csv.read_header(
-                io::ignore_extra_column, "bookID", "title", "language_code");
-            libfts::DocId document_id = 0;
-            libfts::Doc text;
-            std::string language;
-            while (csv.read_row(document_id, text, language)) {
-                if (language == "eng" || language == "en-US") {
-                    indexer.add_document(document_id, text, config);
-                }
-            }
-            libfts::TextIndexWriter::write(
-                c_absolute_path / "index", indexer.get_index());
+            launch_indexer(config, path);
         } else if (searcher_sub->parsed()) {
             if (searcher_sub->count("--query") != 1) {
-                replxx::Replxx editor;
-                fmt::print("input query text\n");
-                query = editor.input("> ");
+                launch_interactive_searcher(config, path, query);
+            } else {
+                launch_searcher(config, path, query);
             }
-            libfts::IndexBuilder indexer;
-            indexer.read(c_absolute_path / "index");
-            libfts::IndexAccessor accessor(indexer.get_index());
-            auto result = libfts::search(query, config, accessor);
-            fmt::print("{}", libfts::get_string_search_result(result));
         }
     } catch (libfts::ConfigurationException &ex) {
         fmt::print("parse error: {}\n", ex.what());
-        return -1;
-    } catch (libfts::AccessorException &ex) {
-        fmt::print("error: {}\n", ex.what());
         return -1;
     };
     return 0;
