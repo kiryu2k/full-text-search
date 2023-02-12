5aeb7a9e31b664fbbad03892c690d71a2c4b7258
wakaisera <sailor.kirill@gmail.com>
2023-02-12 18:10:15 +0700

    Update python client app

--- pythonscripts/fts.py
+++ pythonscripts/fts.py
@@ -1,37 +1,53 @@
 import ctypes
 
-api_loader = ctypes.CDLL("pythonscripts/libsearchapi.so")
+search_lib = ctypes.CDLL("pythonscripts/libsearchapi.so")
 
 # create parser config object
-api_loader.parser_configuration_new.restype = ctypes.c_void_p
-
-# load configuration from file
-api_loader.parser_configuration_load.argtypes = [
-    ctypes.c_void_p, ctypes.c_char_p]
+parser_config_new = search_lib.parser_configuration_new
+parser_config_new.restype = ctypes.c_void_p
+parser_config_new.argtypes = [ctypes.c_char_p]
 
 # delete config object
-api_loader.parser_configuration_delete.argtypes = [ctypes.c_void_p]
-
-# create index object
-api_loader.index_new.restype = ctypes.c_void_p
-
-# read text index
-api_loader.text_index_read.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-
-# delete index object
-api_loader.index_delete.argtypes = [ctypes.c_void_p]
+parser_config_del = search_lib.parser_configuration_delete
+parser_config_del.argtypes = [ctypes.c_void_p]
 
 # create index accessor object
-api_loader.index_accessor_new.restype = ctypes.c_void_p
-api_loader.index_accessor_new.argtypes = [ctypes.c_void_p]
+index_accessor_new = search_lib.index_accessor_new
+index_accessor_new.restype = ctypes.c_void_p
+index_accessor_new.argtypes = [ctypes.c_char_p]
 
 # delete index accessor object
-api_loader.index_accessor_delete.argtypes = [ctypes.c_void_p]
+index_accessor_del = search_lib.index_accessor_delete
+index_accessor_del.argtypes = [ctypes.c_void_p]
 
 # launch searcher
-api_loader.search.restype = ctypes.c_void_p
-api_loader.search.argtypes = [
+search_lib.search.restype = ctypes.c_void_p
+search_lib.search.argtypes = [
     ctypes.c_char_p, ctypes.c_void_p, ctypes.c_void_p]
 
 # free search result memory
-api_loader.search_result_delete.argtypes = [ctypes.c_char_p]
+search_lib.search_result_delete.argtypes = [ctypes.c_char_p]
+
+
+class ParserConfiguration:
+    def __init__(self, path: str) -> None:
+        self.obj = parser_config_new(path.encode("utf-8"))
+
+    def __del__(self) -> None:
+        parser_config_del(self.obj)
+
+
+class IndexAccessor:
+    def __init__(self, index_dir: str) -> None:
+        self.obj = index_accessor_new(index_dir.encode("utf-8"))
+
+    def __del__(self) -> None:
+        index_accessor_del(self.obj)
+
+
+def search(query: str, config: ParserConfiguration, index: IndexAccessor):
+    ctypes_result = ctypes.c_char_p(search_lib.search(
+        query.encode("utf-8"), config.obj, index.obj))
+    result = str(ctypes_result.value.decode("utf-8"))
+    search_lib.search_result_delete(ctypes_result)
+    return result
