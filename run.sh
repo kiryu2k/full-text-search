1a7e81497bd65af282211d9daa88e4060073f542
wakaisera <sailor.kirill@gmail.com>
2023-02-14 14:07:15 +0700

    Add script to configure, build and run app

---
+++ run.sh
@@ -0,0 +1,8 @@
+DEBUG_DIR=/build/build
+if [ ! -d "$DEBUG_DIR" ];
+then
+    cmake --preset debug
+fi
+cmake --build --preset debug
+./build/debug/bin/app index -i index -c books.csv
+python3 pythonscripts/main.py -i index
